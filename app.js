0c829df86bae419e2ab482177e9cd23b707c1362
JiangShuuu <globelex65@gmail.com>
2022-04-21 20:14:34 +0800

    add first

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
 const express = require('express')
+const mongoose = require('mongoose') 
+
+const MONGODB_URI = "mongodb+srv://globelex65:Globelex1418@cluster0.a4qoq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+
 const app = express()
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
+
+// 取得資料庫連線狀態
+const db = mongoose.connection
+
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 // 設定路由
 app.get('/', (req, res) => {
