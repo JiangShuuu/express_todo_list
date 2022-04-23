40e75c9fa4283e7ae13062b4202b86c56f589774
JiangShuuu <globelex65@gmail.com>
2022-04-23 09:37:56 +0800

    f: editPage

--- app.js
+++ app.js
@@ -41,6 +41,7 @@ app.get('/todos/new', (req, res) => {
   return res.render('new')
 })
 
+// 創造資料
 app.post('/todos', (req, res) => {
   const name = req.body.name
 
@@ -61,13 +62,33 @@ app.post('/todos', (req, res) => {
   */
 })
 
-// 設定middleware
 app.get('/todos/:id', (req, res) => {
   const id = req.params.id
   return Todo.findById(id)
     .lean()
     .then(todo => res.render('detail', { todo }))
     .catch(err => console.log(err))
+})
+
+// edit頁面
+app.get('/todos/:id/edit', (req, res) => {
+  const id = req.params.id
+  return Todo.findById(id)
+    .lean()
+    .then(todo => res.render('edit', { todo }))
+    .catch(err => console.log(err))
+})
+
+app.post('/todos/:id/edit', (req, res) => {
+  const id = req.params.id
+  const name = req.body.name
+  return Todo.findById(id)
+    .then(todo => {
+      todo.name = name
+      return todo.save()
+    })
+    .then(() => res.redirect(`/todos/${id}`))
+    .catch(error => console.log(error))
 })
 
 // 設定 port 3000
